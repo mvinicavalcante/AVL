b43e356eda0dd46a331b6e63531a3e863a251533
Marcos Vinícius Cavalcante Oliveira <mvinic2011@gmail.com>
2023-12-08 00:13:01 -0300

    Estrutura de inserção

--- avl.c
+++ avl.c
@@ -11,5 +11,66 @@ arvore inserir(arvore raiz, int valor, int *cresceu) {
         nova->fb = 0;
         *cresceu = 1;
         return nova;
+    } else {
+        if(valor > raiz->valor) {
+            raiz->dir = inserir(raiz->dir, valor);
+            if(*cresceu) {
+                switch(raiz->fb) {
+                    case 0:
+                        raiz->fb = 1;
+                        *cresceu = 1;
+                        break;
+                    case 1:
+                        *cresceu = 0; //porque ficaria desbalanceado, ele zera
+                        //o crescimento pq faz a rotação
+                        return rotacao(raiz);
+                        break;
+                    case -1:
+                        *cresceu = 0;
+                        break;
+                }
+            }
+        } else {
+            raiz->esq = inserir(raiz->esq, valor);
+            if(*cresceu) {
+                switch(raiz->fb) {
+                case 0:
+                    raiz->fb = -1;
+                    *cresceu = 1;
+                    break;
+                case 1:
+                    *cresceu = 0;
+                    break;
+                case -1:
+                    *cresceu = 0;
+                    return rotacao(raiz);
+                    break;
+                }
+            }
+        }
+    }
+    return raiz;
+}
+
+arvore rotacao(arvore pivo) {
+    if(pivo->fb > 0) {
+    //rotação esquerda
+        if(pivo->dir->fb >= 0) {
+            if(pivo->dir->fb == 0) {
+                pivo->fb = 1;
+                pivo->dir->fb = -1;
+            } else {
+                pivo->fb = 0;
+                pivo->dir->fb = 0;
+            }
+        } else {
+            pivo->fb = -1;
+            pivo->dir->fb = 1;
+        }
+
+    } else {
+
     }
 }
+
+
